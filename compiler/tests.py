f5c3600e4b5ee4e1a892d705723657f8315c1bbe
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-01-17 18:06:19 +0100

    Added functionCalls test

--- compiler/tests.py
+++ compiler/tests.py
@@ -19,3 +19,34 @@ def helloWorld():
     }, Function([], [
         Call("helloWorld", []) # helloWorld()
     ], None))
+
+def functionCalls():
+    # external puts as print
+    # def reprint(one:String, two:String) {
+    #     print(one)
+    #     return two
+    #     print(two)
+    # }
+    # print(reprint("1", "2"))
+    #
+    #=> 1\n2\n
+
+    return Module( {
+        "print" : ExternalFunction("puts", [LLVMType("String")], LLVMType("Int")), # external puts as print
+        "reprint": Function([Variable("one", LLVMType("String")), Variable("two", LLVMType("String"))],
+            Call("print", [
+                Reference("one")
+            ] ),
+            Return(Reference("two")),
+            Call("print", [
+                Reference("two")
+            ] )
+        )
+    }, Function([], [
+        Call("print", [
+            Call("reprint", [
+                Literal("1", LLVMType("String")),
+                Literal("2", LLVMType("String"))
+            ] )
+        ] )
+    ] ))
