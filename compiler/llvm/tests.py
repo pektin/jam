4ae13938d9aac9935bd9b63aaa338482c76613b7
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-01-17 05:45:13 +0100

    Made llvm testing automated using clang

--- compiler/llvm/tests.py
+++ compiler/llvm/tests.py
@@ -1,12 +1,14 @@
 import unittest
 from io import StringIO
+from tempfile import NamedTemporaryFile
+import os, subprocess
 
 from ..lekvar.lekvar import *
 from .emitter import *
 
 class LLVMEmitterTest(unittest.TestCase):
     def helloWorld(self): #TODO: Standardise tests
-        return Module( {
+        test = Module( {
             "helloWorld": Function([], [
                 Call("print", [
                     Literal( LLVMType("String"), "Hello World!" )
@@ -16,12 +18,22 @@ class LLVMEmitterTest(unittest.TestCase):
         }, Function([], [
             Call("helloWorld", [])
         ], []))
+        test.verify()
+        return test
 
     def test_helloWorld(self):
         test = self.helloWorld()
-        test.verify()
-        output = StringIO()
-        emitter = Emitter(output)
-        test.emitDefinition(emitter)
-        emitter.finalize()
-        print(output.getvalue())
+
+        # Compile and run the generate llvm ir
+        os.makedirs("builds", exist_ok=True)
+        with open("builds/helloWorld.ll", "w") as f:
+            emitter = Emitter(f)
+            test.emitDefinition(emitter)
+            emitter.finalize()
+
+        # Compile with clang, asserting no errors are found
+        self.assertEqual(subprocess.call(["clang -o builds/helloWorld builds/helloWorld.ll"], shell=True), 0)
+        # test output
+        output = subprocess.check_output(["builds/helloWorld"]).decode("utf-8")
+        print(output)
+        self.assertEqual(output, "Hello World!\n")
