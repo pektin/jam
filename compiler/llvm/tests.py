e45a45b63660d90f2806e657751f445067ea9191
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-01-18 22:53:35 +0100

    Added functionCalls llvm-ir emission test

--- compiler/llvm/tests.py
+++ compiler/llvm/tests.py
@@ -8,19 +8,27 @@ from .emitter import *
 from .. import tests
 
 class LLVMEmitterTest(unittest.TestCase):
-    def test_helloWorld(self):
-        test = tests.helloWorld()
-        test.verify()
-
+    def compileRun(self, test, name):
         # Compile and run the generate llvm ir
         os.makedirs("builds", exist_ok=True)
-        with open("builds/helloWorld.ll", "w") as f:
+        with open("builds/{}.ll".format(name), "w") as f:
             emitter = Emitter(f)
             test.emitDefinition(emitter)
             emitter.finalize()
 
         # Compile with clang, asserting no errors are found
-        self.assertEqual(subprocess.call(["clang -o builds/helloWorld builds/helloWorld.ll"], shell=True), 0)
+        self.assertEqual(subprocess.call(
+            ["clang -o builds/{} builds/{}.ll".format(name, name)
+        ], shell=True), 0)
         # test output
-        output = subprocess.check_output(["builds/helloWorld"]).decode("utf-8")
-        self.assertEqual(output, "Hello World!\n")
+        return subprocess.check_output(["builds/{}".format(name)]).decode("utf-8")
+
+    def test_helloWorld(self):
+        test = tests.helloWorld()
+        test.verify()
+        self.assertEqual(self.compileRun(test, "helloWorld"), "Hello World!\n")
+
+    def test_functionCalls(self):
+        test = tests.functionCalls()
+        test.verify()
+        self.assertEqual(self.compileRun(test, "functionCalls"), "1\n2\n")
