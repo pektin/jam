c4d2bdaab38782a0237e12138690151df9c3f049
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-02-02 20:05:55 +0100

    Added missing formatting of version number into html title

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Jam v{} Docs"
+html_title = "Jam v{} Docs".format(version)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
