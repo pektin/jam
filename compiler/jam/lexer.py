10737ce45bc5449df2f14f9977b04637ef9a5b4c
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-02-07 08:54:46 +1100

    Added comment lexing to jam

--- compiler/jam/lexer.py
+++ compiler/jam/lexer.py
@@ -1,11 +1,19 @@
 from enum import Enum
 
+from ..errors import *
+
 Tokens = Enum("Tokens", [
     "comment"
     "identifier"
     "keyword"
     "string"
 ])
+
+#
+# Constants
+#
+
+COMMENT_CHAR = "#"
 
 class Token:
     def __init__(self, type:Tokens, start:int, end:int, data:str):
@@ -20,6 +28,31 @@ class Lexer:
     def __init__(self, source:file):
         self.source = source
 
+    @property
+    def pos(self):ok
+        return source.tell()
+
+    def read(self):
+        return source.read(1)
+
+    #
+    # Lexing Methods
+    #
+
     def next(self):
-        #TODO: Actual lexing
+        current = self.pop()
+
+        if current == COMMENT_CHAR:
+            return self.comment()
+        else:
+            raise SyntaxError("Unexpected Character '{}'".format(current))
+
+    def comment(self):
+        start = self.pos - 1
+
+        comment = self.pop()
+        while comment[-1] != "\n":
+            comment += self.pop()
+
+        return Token(Tokens.comment, start, self.pos - 1, comment)
 
