45fe447e2aed60a8ca48283e2c0c99aea662add3
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-02-08 22:44:44 +1100

    Made parsing more robust and fixed current parsing errors

--- compiler/jam/parser.py
+++ compiler/jam/parser.py
@@ -19,7 +19,10 @@ def parseFile(source:IOBase):
         # EOF escape
         if token is None: break
 
-        value = parseValue(lexer, token)
+        value = parseLine(lexer, token)
+
+        # EOF escape
+        if value is None: break
 
         if isinstance(value, lekvar.Scope):
             # Scopes are automatically added as children
@@ -28,13 +31,33 @@ def parseFile(source:IOBase):
             # Other values are added as instructions
             instructions.append(value)
 
-    return lekvar.Module( children, Function([], instructions, None) )
+    return lekvar.Module( children, lekvar.Function([], instructions, None) )
 
 def parseLine(lexer, token):
+    # Parse a line. The line may not exist
+
+    # Ignore newlines
+    while token.type == Tokens.newline:
+        token = lexer.lex()
+
+        # EOF handling
+        if token is None: return None
+
     if token.type == Tokens.comment:
-        return lekvar.Comment()
+        return lekvar.Comment(token.data)
     else:
         return parseValue(lexer, token)
 
 def parseValue(lexer, token):
+    # Parse for a value. The value must exist
+
+    # Ignore comments and newlines until a value is reached
+    while token.type in [Tokens.comment, Tokens.newline]:
+        token = lexer.lex()
+
+        # EOF handling
+        if token is None:
+            raise SyntaxError("Expected value before EOF")
+
+    #TODO: Everything
     raise NotImplemented()
