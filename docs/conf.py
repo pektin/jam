ad5ec2611f0e73aef68541bf00a295a1137e118a
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-02-20 18:11:44 +1100

    Changed documentation to basic theme

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ pygments_style = 'sphinx'
 keep_warnings = True
 
 # The theme to use for HTML and HTML Help pages.
-html_theme = 'default'
+html_theme = 'basic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
