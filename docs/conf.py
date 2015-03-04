f275bd7775b8c2ebab9fbc1211c484dea9bbdf64
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-03-04 11:49:27 +1100

    Removed unnecessary sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,7 @@ import os
 
 extensions = [
     'sphinx.ext.todo',
-    'sphinx.ext.ifconfig',
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -17,7 +13,6 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Jam'
-copyright = '2015, Benjamin Schaaf'
 
 # The short X.Y version.
 version = '0.0'
@@ -32,62 +27,3 @@ keep_warnings = True
 
 # The theme to use for HTML and HTML Help pages.
 html_theme = 'basic'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-html_title = "Jam v{} Docs".format(version)
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-#html_logo = None
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-#html_favicon = None
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-#html_extra_path = []
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# No module index is generated.
-html_domain_indices = False
-
-# No index is generated.
-html_use_index = False
-
-# If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = True
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'jam-doc'
