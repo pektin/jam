f297653c25375654bd65ffb589a2f0ecb086093d
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-03-10 11:12:03 +1100

    Changed state handling to use a static class

--- compiler/llvm/emitter.py
+++ compiler/llvm/emitter.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import contextmanager
 from functools import partial
 from abc import abstractmethod as abstract
@@ -8,39 +9,46 @@ from ..errors import *
 from . import bindings as llvm
 from .builtins import LLVMType
 
-def emit(module:lekvar.Module):
-    state = State(b"main")
-    module.emit(state)
-    state.finalize()
-    return state.module.toString()
+def emit(module:lekvar.Module, logger:logging.Logger = None):
+    if logger is None: logger = logging.getLogger()
+
+    with State.begin(b"main", logger):
+        module.emit()
+
+    return State.module.toString()
 
 class State:
-    def __init__(self, name:str):
-        self.builder = llvm.Builder.new()
-        self.module = llvm.Module.fromName(name)
-        self.main = []
+    @classmethod
+    @contextmanager
+    def begin(cls, name:str, logger:logging.Logger):
+        cls.logger = logger
 
-    def finalize(self):
+        cls.builder = llvm.Builder.new()
+        cls.module = llvm.Module.fromName(name)
+        cls.main = []
+        yield
         main_type = llvm.Function.new(llvm.Int.new(32), [], False)
-        main = self.module.addFunction("main", main_type)
+        main = cls.module.addFunction("main", main_type)
 
         entry = main.appendBlock("entry")
-        with self.blockScope(entry):
-            for func in self.main:
+        with cls.blockScope(entry):
+            for func in cls.main:
                 call = lekvar.Call("", [])
                 call.called = func
-                call.emitValue(self)
+                call.emitValue()
 
             return_value = llvm.Value.constInt(llvm.Int.new(32), 0, False)
-            self.builder.ret(return_value)
+            cls.builder.ret(return_value)
 
+    @classmethod
     @contextmanager
-    def blockScope(self, block:llvm.Block):
-        previous_block = self.builder.position
-        self.builder.positionAtEnd(block)
+    def blockScope(cls, block:llvm.Block):
+        previous_block = cls.builder.position
+        cls.builder.positionAtEnd(block)
         yield
-        self.builder.positionAtEnd(previous_block)
+        cls.builder.positionAtEnd(previous_block)
 
+    @classmethod
     def getTempName(self):
         return "temp"
 
@@ -87,7 +95,7 @@ def resolveName(scope:lekvar.Scope):
 # Implements
 
 # For this that don't emit anything
-def blank_emitValue(self, state:State):
+def blank_emitValue(self):
     return None
 lekvar.Comment.emitValue = blank_emitValue
 
@@ -95,24 +103,24 @@ lekvar.Comment.emitValue = blank_emitValue
 # class Reference
 #
 
-def Reference_emitValue(self, state:State):
-    return self.value.emitValue(state)
+def Reference_emitValue(self):
+    return self.value.emitValue()
 lekvar.Reference.emitValue = Reference_emitValue
 
-def Reference_emitType(self, state:State):
+def Reference_emitType(self):
     if self.value is None: print("Missing:", self)
-    return self.value.emitType(state)
+    return self.value.emitType()
 lekvar.Reference.emitType = Reference_emitType
 
 #
 # class Literal
 #
 
-def Literal_emitValue(self, state:State):
-    type = self.type.emitType(state)
+def Literal_emitValue(self):
+    type = self.type.emitType()
 
     if type == LLVM_MAP["String"]:
-        return state.builder.globalString(self.data, state.getTempName())
+        return State.builder.globalString(self.data, State.getTempName())
     else:
         raise NotImplemented()
 lekvar.Literal.emitValue = Literal_emitValue
@@ -121,110 +129,109 @@ lekvar.Literal.emitValue = Literal_emitValue
 # class Variable
 #
 
-def Variable_emit(self, state:State):
+def Variable_emit(self):
     if self.llvm_value is not None: return
 
-    type = self.type.emitType(state)
+    type = self.type.emitType()
     name = resolveName(self)
-    self.llvm_value = state.builder.alloca(type, name)
+    self.llvm_value = State.builder.alloca(type, name)
 #lekvar.Variable.emit = Variable_emit
 
-def Variable_emitValue(self, state:State):
-    self.emit(state)
-    return state.builder.load(self.llvm_value, state.getTempName())
+def Variable_emitValue(self):
+    self.emit()
+    return State.builder.load(self.llvm_value, State.getTempName())
 #lekvar.Variable.emitValue = Variable_emitValue
 
 #
 # class Assignment
 #
 
-def Assignment_emitValue(self, state:State):
-    value = self.value.emitValue(state)
-    self.variable.emit(state)
-    state.builder.store(value, self.variable.llvm_value)
+def Assignment_emitValue(self):
+    value = self.value.emitValue()
+    self.variable.emit()
+    State.builder.store(value, self.variable.llvm_value)
 #lekvar.Assignment.emitValue = Assignment_emitValue
 
 #
 # class Module
 #
 
-def Module_emit(self, state:State):
+def Module_emit(self):
     if self.llvm_value is not None: return
 
-    self.llvm_value = self.main.emitValue(state)
-    state.main.append(self.main)
+    self.llvm_value = self.main.emitValue()
+    State.main.append(self.main)
 
     for child in self.children.values():
-        print(child)
-        child.emit(state)
+        child.emit()
 lekvar.Module.emit = Module_emit
 
 #
 # class Call
 #
 
-def Call_emitValue(self, state:State):
-    arguments = [val.emitValue(state) for val in self.values]
+def Call_emitValue(self):
+    arguments = [val.emitValue() for val in self.values]
     if self.called.type.return_type is None:
         name = ""
     else:
-        name = state.getTempName()
-    return state.builder.call(self.called.emitValue(state), arguments, name)
+        name = State.getTempName()
+    return State.builder.call(self.called.emitValue(), arguments, name)
 lekvar.Call.emitValue = Call_emitValue
 
 #
 # class Return
 #
 
-def Return_emitValue(self, state:State):
+def Return_emitValue(self):
     exit = self.parent.llvm_value.getLastBlock()
     if self.value is None:
-        state.builder.br(return_)
+        State.builder.br(return_)
     else:
-        value = self.value.emitValue(state)
-        state.builder.store(value, self.parent.llvm_return)
-        state.builder.br(exit)
+        value = self.value.emitValue()
+        State.builder.store(value, self.parent.llvm_return)
+        State.builder.br(exit)
 #lekvar.Return.emitValue = Return_emitValue
 
 #
 # class Function
 #
 
-def Function_emit(self, state:State):
+def Function_emit(self):
     if self.llvm_value is not None: return
 
     name = resolveName(self)
-    func_type = self.resolveType(self).emitType(state)
-    self.llvm_value = state.module.addFunction(name, func_type)
+    func_type = self.resolveType(self).emitType()
+    self.llvm_value = State.module.addFunction(name, func_type)
 
     entry = self.llvm_value.appendBlock("entry")
     exit = self.llvm_value.appendBlock("exit")
-    with state.blockScope(entry):
+    with State.blockScope(entry):
         # Allocate Arguments
         for index, arg in enumerate(self.arguments):
             val = self.llvm_value.getParam(index)
-            arg.llvm_value = state.builder.alloca(arg.type.emitType(state), resolveName(arg))
-            state.builder.store(val, arg.llvm_value)
+            arg.llvm_value = State.builder.alloca(arg.type.emitType(), resolveName(arg))
+            State.builder.store(val, arg.llvm_value)
 
         # Allocate Return Variable
         if self.type.return_type is not None:
-            self.llvm_return = state.builder.alloca(self.type.return_type.emitType(state), "return")
+            self.llvm_return = State.builder.alloca(self.type.return_type.emitType(), "return")
 
         for instruction in self.instructions:
-            instruction.emitValue(state)
+            instruction.emitValue()
 
-        state.builder.br(exit)
+        State.builder.br(exit)
 
-    with state.blockScope(exit):
+    with State.blockScope(exit):
         if self.llvm_return is not None:
-            val = state.builder.load(self.llvm_return, state.getTempName())
-            state.builder.ret(val)
+            val = State.builder.load(self.llvm_return, State.getTempName())
+            State.builder.ret(val)
         else:
-            state.builder.retVoid()
+            State.builder.retVoid()
 lekvar.Function.emit = Function_emit
 
-def Function_emitValue(self, state:State):
-    self.emit(state)
+def Function_emitValue(self):
+    self.emit()
     return self.llvm_value
 lekvar.Function.emitValue = Function_emitValue
 
@@ -232,10 +239,10 @@ lekvar.Function.emitValue = Function_emitValue
 # class FunctionType
 #
 
-def FunctionType_emitType(self, state:State):
-    arguments = [type.emitType(state) for type in self.arguments]
+def FunctionType_emitType(self):
+    arguments = [type.emitType() for type in self.arguments]
     if self.return_type is not None:
-        return_type = self.return_type.emitType(state)
+        return_type = self.return_type.emitType()
     else:
         return_type = llvm.Type.void()
     return llvm.Function.new(return_type, arguments, False)
@@ -245,16 +252,15 @@ lekvar.FunctionType.emitType = FunctionType_emitType
 # class ExternalFunction
 #
 
-def ExternalFunction_emit(self, state:State):
+def ExternalFunction_emit(self):
     if self.llvm_value is not None: return
 
-    func_type = self.type.emitType(state)
-    print(self)
-    self.llvm_value = state.module.addFunction(self.external_name, func_type)
+    func_type = self.type.emitType()
+    self.llvm_value = State.module.addFunction(self.external_name, func_type)
 lekvar.ExternalFunction.emit = ExternalFunction_emit
 
-def ExternalFunction_emitValue(self, state:State):
-    self.emit(state)
+def ExternalFunction_emitValue(self):
+    self.emit()
     return self.llvm_value
 lekvar.ExternalFunction.emitValue = ExternalFunction_emitValue
 
@@ -262,11 +268,11 @@ lekvar.ExternalFunction.emitValue = ExternalFunction_emitValue
 # class Method
 #
 
-def Method_emit(self, state:State):
+def Method_emit(self):
     if self.llvm_value is not None: return
 
     for overload in self.overloads:
-        overload.emit(state)
+        overload.emit()
 lekvar.Method.emit = Method_emit
 
 #
@@ -275,7 +281,7 @@ lekvar.Method.emit = Method_emit
 
 LLVM_MAP = None
 
-def LLVMType_emitType(self, state:State):
+def LLVMType_emitType(self):
     global LLVM_MAP
     if LLVM_MAP is None:
         LLVM_MAP = {
