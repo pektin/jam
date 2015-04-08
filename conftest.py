0eb7a7c9c63b206d42d7835e4305f26d36e8ca13
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-04-08 18:09:23 +1000

    Fixed misstype error

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
 def pytest_generate_tests(metafunc):
-    # Pass the py.test -v fag to any verbosity parameters for tests
+    # Pass the py.test -v flag to any verbosity parameters for tests
     if "verbosity" in metafunc.fixturenames:
         metafunc.parametrize("verbosity", [metafunc.config.option.verbose])
