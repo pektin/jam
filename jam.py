75f43a2ca1574d92cfe4376d68fe95b827c134fa
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-04-14 17:32:35 +1000

    Improved compiler script to support all basic functionality

--- jam.py
+++ jam.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os
+import sys
+import logging
 import argparse
+from io import StringIO
 
-from compiler.jam.compiler import compile
+from compiler.jam import compiler
 
 parser = argparse.ArgumentParser(
     prog = "jam",
     description = "The Jam Compiler",
 )
 parser.add_argument("input",
-    help="The source file to compile",
+    help="The source file to compile.",
     metavar="FILE",
     type=argparse.FileType('r'),
 )
 parser.add_argument("--verbose", "-v",
-    help="Enable debug printing (Doesn't work yet)",
+    help="Enable debug logging at a specific level.",
     action='count',
     required=False,
+    default=0,
 )
 parser.add_argument("--output", "-o",
-    help="The file to compile to",
+    help="The file to compile to.",
     type=argparse.FileType('w'),
     required=False,
     default=None,
 )
-parser.add_argument("--run", "-r",
-    help="Run the compiled file (Doesn't work yet)",
+parser.add_argument("--norun", "-r",
+    help="Only compile the file, don't run it. Use in conjunction with -o to compile to a target.",
     action='store_true',
     required=False,
 )
 parser.add_argument("--version",
-    help="Prints the version of the program",
+    help="Prints the version of the program.",
     action='version',
     version="Jam Compiler V0.1a",
 )
@@ -39,12 +43,14 @@ parser.add_argument("--version",
 def main():
     args = parser.parse_args()
 
-    if args.output is None:
-        path = os.path.splitext(os.path.basename(args.input.name))[0] + ".ll"
-        args.output = open(path, "w")
+    output = args.output or open(os.devnull, "w")
 
-    compile(args.input, args.output)
-    #TODO: Implement other functionality
+    compile = compiler.compile if args.norun else compiler.compileRun
+
+    logging.basicConfig(level=logging.WARNING - args.verbose*10, stream=sys.stdout)
+
+    with args.input, output:
+        print(compile(args.input, output))
 
 if __name__ == "__main__":
     main()
