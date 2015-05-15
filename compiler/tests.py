bfcac93407d3497e72af4f068faa9aea4fd04fa6
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-05-15 20:24:57 +1000

    Made tests not create output file for expected error tests

--- compiler/tests.py
+++ compiler/tests.py
@@ -25,7 +25,7 @@ for file in os.listdir(TESTS_PATH):
         os.makedirs(BUILD_PATH, exist_ok=True)
 
         # Get output data
-        with open(file, "r") as f_in, open(build, "w") as f_out:
+        with open(file, "r") as f_in:
             # The first character is the test type
             type = f_in.read(1)
             # Or specific test attributes (ignore for now)
@@ -35,15 +35,16 @@ for file in os.listdir(TESTS_PATH):
             # The first line is the output
             output = f_in.readline()[:-1].encode("UTF-8").decode("unicode-escape")
 
-            # Check if the output was correct
-            if type == "#":
-                assert output == compileRun(f_in, f_out)
-            # Check if the correct exception was thrown
-            elif type == "!":
-                with pytest.raises(getattr(errors, output)):
-                    compile(f_in, f_out)
-            else:
-                raise errors.InternalError("Invalid Test Output Type: {}".format(type))
+            with open(build, "w") as f_out:
+                # Check if the output was correct
+                if type == "#":
+                    assert output == compileRun(f_in, f_out)
+                # Check if the correct exception was thrown
+                elif type == "!":
+                    with pytest.raises(getattr(errors, output)):
+                        compile(f_in, f_out)
+                else:
+                    raise errors.InternalError("Invalid Test Output Type: {}".format(type))
 
     # get test attributes
     with open(file, "r") as f:
