eb338d12c8f612b7e84afdb00e09a6ba924d82a9
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-05-24 12:23:13 +1000

    Organised full stack tests into subfolders.

--- compiler/tests.py
+++ compiler/tests.py
@@ -1,7 +1,8 @@
 import os, sys
-import pytest
+import fnmatch
 import logging
-from subprocess import check_output
+
+import pytest
 
 from . import errors
 from .jam.compiler import compile, compileRun
@@ -9,48 +10,49 @@ from .jam.compiler import compile, compileRun
 TESTS_PATH = os.path.join("compiler", "tests")
 BUILD_PATH = os.path.join("build", "tests")
 
-for file in os.listdir(TESTS_PATH):
-    file = os.path.join(TESTS_PATH, file)
+for root, dirs, files in os.walk(TESTS_PATH):
 
-    # Ignore non-jam files
-    if os.path.splitext(file)[1] != ".jm": continue
+    # All jam files are tests
+    for file in fnmatch.filter(files, "*.jm"):
+        path = os.path.join(root, file)
+        name = "test_" + os.path.split(root)[1] + "." + file
 
-    def test(verbosity, file=file):
-        logging.basicConfig(level=logging.WARNING - verbosity*10, stream=sys.stdout)
+        def test(file=file, path=path, verbosity=0):
+            logging.basicConfig(level=logging.WARNING - verbosity*10, stream=sys.stdout)
 
-        # Get the path to the built file
-        build = os.path.join(BUILD_PATH, os.path.basename(file).replace(".jm", ".ll"))
+            # Get the path to the built file
+            build = os.path.join(BUILD_PATH, file.replace(".jm", ".ll"))
 
-        # Make the testing directories, if needed
-        os.makedirs(BUILD_PATH, exist_ok=True)
+            # Make the testing directories, if needed
+            os.makedirs(BUILD_PATH, exist_ok=True)
 
-        # Get output data
-        with open(file, "r") as f_in:
-            # The first character is the test type
-            type = f_in.read(1)
-            # Or specific test attributes (ignore for now)
-            if type == "?":
+            # Get output data
+            with open(path, "r") as f_in:
+                # The first character is the test type
                 type = f_in.read(1)
+                # Or specific test attributes (ignore for now)
+                if type == "?":
+                    type = f_in.read(1)
 
-            # The first line is the output
-            output = f_in.readline()[:-1].encode("UTF-8").decode("unicode-escape")
+                # The first line is the output
+                output = f_in.readline()[:-1].encode("UTF-8").decode("unicode-escape")
 
-            with open(build, "w") as f_out:
-                # Check if the output was correct
-                if type == "#":
-                    assert output == compileRun(f_in, f_out)
-                # Check if the correct exception was thrown
-                elif type == "!":
-                    with pytest.raises(getattr(errors, output)):
-                        compile(f_in, f_out)
-                else:
-                    raise errors.InternalError("Invalid Test Output Type: {}".format(type))
+                with open(build, "w") as f_out:
+                    # Check if the output was correct
+                    if type == "#":
+                        assert output == compileRun(f_in, f_out)
+                    # Check if the correct exception was thrown
+                    elif type == "!":
+                        with pytest.raises(getattr(errors, output)):
+                            compile(f_in, f_out)
+                    else:
+                        raise errors.InternalError("Invalid Test Output Type: {}".format(type))
 
-    # get test attributes
-    with open(file, "r") as f:
-        if f.read(1) == "?":
-            test = pytest.mark.xfail(test)
+        # get test attributes
+        with open(path, "r") as f:
+            if f.read(1) == "?":
+                test = pytest.mark.xfail(test)
 
-    # "add" test
-    globals()["test_" + file] = test
+        globals()[name] = test
+
 del test
