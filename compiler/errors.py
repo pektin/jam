4888465e303dfd5de9adc09bca1aa80c07f434e5
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-06-19 17:30:40 +1000

    Rewrote error formatting to be more robust and faster

--- compiler/errors.py
+++ compiler/errors.py
@@ -1,3 +1,60 @@
+class _Token:
+    start = 0
+    end = 0
+
+def formatTokens(source:str, tokens:[_Token]):
+    # a mapping of line numbers to a line and set of positions in that line
+    lines = {}
+
+    for token in tokens:
+        line = _getLine(source, token.start)
+
+        if line.number not in lines:
+            lines[line.number] = (line, set())
+
+        for position in range(token.start, token.end):
+            lines[line.number][1].add(position)
+
+    out = []
+
+    for number, (line, highlights) in sorted(lines.items(), key=lambda a: a[0]):
+        number_str = str(number)
+        out.append("{}| {}{}| {}".format(
+            number_str,
+            source[line.start:line.end],
+            " " * len(number_str),
+            "".join(("^" if index in highlights else " ")
+                for index in range(line.start, line.end))
+        ))
+
+    return "\n".join(out)
+
+def _getLine(source:str, position:int):
+    class Line:
+        def __init__(self, number):
+            self.number = number
+            self.start = 0
+            self.end = 0
+
+    line = Line(1)
+
+    # Get the line number and starting index of said line at the position
+    for index, character in enumerate(source):
+        if index == position:
+            break
+        elif character == "\n":
+            line.number += 1
+            line.position = index + 1
+
+    # Get the end index of the line
+    line.end = line.start
+    for index, character in enumerate(source[line.start:]):
+        line.end += 1
+        if character == "\n": # Include the newline
+            break
+
+    return line
+
 # Generic CompilerError
 class CompilerError(Exception):
     # Create a new CompilerError
@@ -5,78 +62,27 @@ class CompilerError(Exception):
     # A compiler error takes a single error message and a list of tokens.
     # When displayed, the error will contain the specified message along with
     # nicely formatted source code extracts, highlighting the specified tokens
-    def __init__(self, err = "", tokens = []):
-        super().__init__(err)
-        self.tokens = tokens
+    def __init__(self, message:str, tokens:[_Token] = None):
+        super().__init__("")
+        self.messages = []
+        if isinstance(message, list):
+            for msg in message:
+                self.addMessage(*msg)
+        else:
+            self.addMessage(message, tokens)
 
-    # Formats the error message given a specified source
-    #
-    # This is presented as a separate function because the error may be
-    # caught multiple times, each adding in related tokens. This allows for more
-    # robust error handling while avoiding massive overhead.
-    #
-    # Compiler errors are designed to be caught by the compiler and formatted
-    # before being returned to the rest of the program
+    def addMessage(self, message:str, tokens:[_Token]):
+        if message or tokens:
+            self.messages.append((message, tokens))
+
     def format(self, source:str):
-
-        # Collect a set of line numbers linked to the contents of that line
-        # and the highlighting of specified tokens on that line
-        lines = {}
-        for token in self.tokens:
-            # Loop through each character position of the token
-            # This might need some optimisation
-            for pos in range(token.start, token.end):
-                number, line, pos = self._getLine(pos, source)
-
-                # Produce new 'lines' entries
-                if number not in lines:
-                    lines[number] = [line, " " * len(line)]
-
-                # Highlight the character
-                lines[number][1] = lines[number][1][:pos - 1] + "^" + lines[number][1][pos:]
-
-        # Produce the final error message
-        out = "\n"
-
-        # Start each source reference with the line number, to match the
-        # behaviour of text editors
-        for number, line in lines.items():
-            line_number = str(number)
-            out += line_number + "| " + line[0]
-            out += len(line_number) * " " + "| " + line[1] + "\n"
-
-        # Set the exceptions error message
-        self.args = (self.args[0] + out,)
-
-    # Given the source code and the position of a character, this function
-    # returns the line number, the line itself and the relative position of the
-    # character in the line that the character is contained in.
-    def _getLine(self, pos:int, source:str):
-        number = 1
-        line = ""
-
-        # Iterate until the character is reached, counting the number of lines
-        # cache each new line encountered
-        for index, char in enumerate(source):
-            if index >= pos:
-                line += char
-
-                if char == "\n":
-                    break
-            elif char == "\n":
-                number += 1
-
-        # Continue from the cached line for the rest of the line the character is in
-        index = pos - 1
-        while index >= 0:
-            if source[index] == "\n":
-                break
-
-            line = source[index] + line
-            index -= 1
-
-        return number, line, pos - index
-
+        message = "\n".join(
+            (msg if msg else "") +
+            ("\n" if msg and tokens else "") +
+            (formatTokens(source, tokens) if tokens else "")
+                for msg, tokens in self.messages
+        )
+        self.args = (message,)
 
 class SyntaxError(CompilerError):
     pass
@@ -96,5 +102,5 @@ class AmbiguityError(SemanticError):
 class MissingReferenceError(SemanticError):
     pass
 
-class InternalError(CompilerError):
+class InternalError(Exception):
     pass
