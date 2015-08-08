29a6f9cf5093e9b63d5b4fac8ed1d19a9f78b0a7
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-08-09 06:44:31 +1000

    fixed llvm tests for working with the new module structure

--- compiler/llvm/tests.py
+++ compiler/llvm/tests.py
@@ -3,22 +3,20 @@ from subprocess import check_output
 
 import pytest
 
-from ..errors import ExecutionError
-
-from . import bindings as llvm
-from .builtins import builtins
-from .emitter import emit, run
+from ..errors import *
+from .. import llvm
+from ..llvm import bindings as c
 
 BUILD_PATH = "build/tests"
 
 def test_llvm():
-    i32 = llvm.Int.new(32)
-    module = llvm.Module.fromName("test")
-    builder = llvm.Builder.new()
+    i32 = c.Int.new(32)
+    module = c.Module.fromName("test")
+    builder = c.Builder.new()
 
-    puts_type = llvm.Function.new(i32, [llvm.Pointer.new(llvm.Int.new(8), 0)], False)
+    puts_type = c.Function.new(i32, [c.Pointer.new(c.Int.new(8), 0)], False)
     puts = module.addFunction("puts", puts_type)
-    main = module.addFunction("main", llvm.Function.new(i32, [], False))
+    main = module.addFunction("main", c.Function.new(i32, [], False))
 
     entry = main.appendBlock("entry")
     return_ = main.appendBlock("return")
@@ -29,7 +27,7 @@ def test_llvm():
     builder.br(return_)
 
     builder.positionAtEnd(return_)
-    builder.ret(llvm.Value.constInt(i32, 0, False))
+    builder.ret(c.Value.constInt(i32, 0, False))
 
     os.makedirs(BUILD_PATH, exist_ok=True)
     with open(BUILD_PATH + "/llvm.ll", "wb") as f:
@@ -38,11 +36,11 @@ def test_llvm():
     assert b"Hello World!\n" == check_output(["lli " + BUILD_PATH + "/llvm.ll"], shell=True)
 
 def test_module_verification_handling():
-    module = llvm.Module.fromName("test")
-    builder = llvm.Builder.new()
+    module = c.Module.fromName("test")
+    builder = c.Builder.new()
 
-    puts = module.addFunction("puts", llvm.Function.new(llvm.Type.void(), [llvm.Int.new(19)], False))
-    main = module.addFunction("main", llvm.Function.new(llvm.Type.void(), [], False))
+    puts = module.addFunction("puts", c.Function.new(c.Type.void(), [c.Int.new(19)], False))
+    main = module.addFunction("main", c.Function.new(c.Type.void(), [], False))
 
     entry = main.appendBlock("")
     builder.positionAtEnd(entry)
@@ -50,7 +48,7 @@ def test_module_verification_handling():
     hello = builder.globalString("Hello World!", "temp.0")
     builder.call(puts, [hello], "")
 
-    with pytest.raises(llvm.VerificationError):
+    with pytest.raises(c.VerificationError):
         module.verify()
 
 def test_lli_failure():
@@ -59,12 +57,11 @@ def rec() { ret rec() }
 """
 
     with pytest.raises(ExecutionError):
-        run(source)
+        llvm.run(source)
 
 def test_builtin_lib():
-    source = emit(builtins())
+    source = llvm.emit(llvm.builtins())
 
     os.makedirs(BUILD_PATH, exist_ok=True)
     with open(BUILD_PATH + "/builtins.ll", "wb") as f:
         f.write(source)
-
