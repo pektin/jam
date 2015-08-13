acccc2ab9c05d433dda9c81ca490b6b42b1fcb55
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-08-13 12:34:12 +1000

    Improved usability of jam compiler script. Can now compile and read from stdin

--- jam.py
+++ jam.py
@@ -7,8 +7,7 @@ import argparse
 import cProfile
 from io import StringIO
 
-import compiler as jam
-from compiler.errors import CompilerError
+from compiler import jam, llvm
 
 parser = argparse.ArgumentParser(
     prog = "jam",
@@ -18,23 +17,22 @@ parser.add_argument("input",
     help="The source file to compile.",
     metavar="FILE",
     type=argparse.FileType('r'),
+    default=sys.stdin,
+    nargs='?',
 )
 parser.add_argument("--verbose", "-v",
     help="Enable debug logging at a specific level.",
     action='count',
-    required=False,
     default=0,
 )
 parser.add_argument("--output", "-o",
     help="The file to compile to.",
     type=argparse.FileType('wb'),
-    required=False,
     default=None,
 )
-parser.add_argument("--norun", "-r",
-    help="Only compile the file, don't run it. Use in conjunction with -o to compile to a target.",
+parser.add_argument("--compile", "-c",
+    help="Instead of running, compile the input.",
     action='store_true',
-    required=False,
 )
 parser.add_argument("--version",
     help="Prints the version of the program.",
@@ -44,32 +42,54 @@ parser.add_argument("--version",
 parser.add_argument("--profile",
     help="Uses cProfile to profile the compiler.",
     action='store_true',
-    required=False,
 )
 
 def main():
     args = parser.parse_args()
 
-    output = args.output or open(os.devnull, "wb")
-
-    jam_fun = jam.compile if args.norun else jam.interpret
-
     logging.basicConfig(level=logging.WARNING - args.verbose*10, stream=sys.stdout)
+
+    # Make sure we always have a compile target when we're compiling
+    if args.compile and args.output is None:
+        if os.path.isfile(args.input.name):
+            name = os.path.splitext(os.path.basename(args.input.name))[0]
+        else:
+            name = "a.out"
+
+        args.output = open(name, "wb")
+
+    # Make the output executable if we're compiling
+    if args.compile and os.path.isfile(args.output.name):
+        os.chmod(args.output.name, 0o775)
 
     if args.profile:
         profiler = cProfile.Profile()
         profiler.enable()
 
-    with args.input, output:
-        try:
-            print(jam_fun(args.input, output), end="")
-        except CompilerError as e:
-            message = e.args[0]
-            print("{}: {}".format(type(e).__name__, message))
+    try:
+        ir = parse(args.input)
 
-    if args.profile:
-        profiler.disable()
-        profiler.print_stats()
+        if args.compile:
+
+            compile(ir, args.output)
+        else:
+            run(ir, args.output)
+    finally:
+        if args.profile:
+            profiler.disable()
+            profiler.print_stats()
+
+def parse(input):
+    return llvm.emit(jam.parse(input))
+
+def compile(ir, output):
+    output.write(llvm.compile(ir))
+
+def run(ir, output):
+    print(llvm.run(ir).decode("UTF-8"), end="")
+
+    if output is not None:
+        output.write(ir)
 
 if __name__ == "__main__":
     main()
