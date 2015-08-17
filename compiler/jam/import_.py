b3320f94a0b820249a537d12bd89b63d743836ba
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-08-17 21:22:12 +1000

    Implemented module importing. module imports test now passes

--- compiler/jam/import_.py
+++ compiler/jam/import_.py
@@ -1,21 +1,38 @@
 from ..errors import *
 
-from ..lekvar.links import Link
 from .. import lekvar
 
-class Import(Link):
+class Import(lekvar.Link, lekvar.BoundObject):
+    static = True
     path = None
 
     def __init__(self, path:[str], name:str = None, tokens = None):
         if name is None:
             name = path[-1]
-        super().__init__(name, None, tokens)
+        super().__init__(None, name)
+        self.tokens = tokens
         self.path = path
+        print(self.value, self.tokens, self.name, self.path)
+
+    @property
+    def local_context(self):
+        return self.value.local_context
 
     def copy(self):
         raise InternalError("Not Implemented")
 
     def verify(self):
-        raise InternalError("Not Implemented")
+        if self.value is not None: return
+
+        try:
+            self.value = lekvar.util.resolveReference(self.path[0])
+            self.path.pop(0)
+        except MissingReferenceError:
+            raise InternalError("Not Implemented")
+
+        for name in self.path:
+            self.value = lekvar.Attribute(self.value, name)
+
+        super().verify()
 
 lekvar.Import = Import
