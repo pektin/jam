fb881dec4711aeeea43ed63b082e8ef5afb7bcb3
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-08-20 10:42:20 +1000

    Implemented logical and/or as builtin library functions. and operator test now passes

--- compiler/jam/builtins.py
+++ compiler/jam/builtins.py
@@ -8,4 +8,29 @@ BUILTINS_PATH = "compiler/jam/builtins.jm"
 
 def builtins(logger = logging.getLogger()):
     ir = parser.parseFile(open(BUILTINS_PATH, "r"), logger)
+
+    # Certain functionality cannot be built into the library
+    # as it is not valid jam
+    and_ = lekvar.Function("&&", [lekvar.Variable("lhs", lekvar.Reference("Bool")),
+                                  lekvar.Variable("rhs", lekvar.Reference("Bool"))],
+    [
+        lekvar.Branch(lekvar.Reference("lhs"), [
+            lekvar.Return(lekvar.Reference("rhs")),
+        ], [
+            lekvar.Return(lekvar.Reference("lhs")),
+        ]),
+    ], [])
+    ir.context.addChild(and_)
+
+    or_ = lekvar.Function("||", [lekvar.Variable("lhs", lekvar.Reference("Bool")),
+                                  lekvar.Variable("rhs", lekvar.Reference("Bool"))],
+    [
+        lekvar.Branch(lekvar.Reference("lhs"), [
+            lekvar.Return(lekvar.Reference("lhs")),
+        ], [
+            lekvar.Return(lekvar.Reference("rhs")),
+        ]),
+    ], [])
+    ir.context.addChild(or_)
+
     return ir
