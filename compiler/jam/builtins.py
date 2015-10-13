118615b653925591bf8a60a47cba73d00523f539
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-10-13 16:55:06 +1100

    Made the path to builtin library relative, allowing the jam compiler tool to be used outside of the jam directory

--- compiler/jam/builtins.py
+++ compiler/jam/builtins.py
@@ -1,19 +1,21 @@
 import io
 import logging
+from os import path
 
 from .. import lekvar
 
 from . import parser
 
-BUILTINS_PATH = "compiler/jam/builtins.jm"
+BUILTINS_FILE = "builtins.jm"
+BUILTINS_PATH = path.join(path.split(__file__)[0], BUILTINS_FILE)
 
 BUILTINS_CACHE = open(BUILTINS_PATH, "r").read()
 
 def builtins(logger = logging.getLogger()):
     ir = parser.parseFile(io.StringIO(BUILTINS_CACHE), logger)
 
-    # Certain functionality cannot be built into the library
-    # as it is not valid jam
+    # Certain functions cannot be built into the library
+    # as they cannot be expressed in jam syntax
     and_ = lekvar.Function("&&", [lekvar.Variable("lhs", lekvar.Reference("Bool")),
                                   lekvar.Variable("rhs", lekvar.Reference("Bool"))],
     [
