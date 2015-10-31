3f10ca5f839c9b5e9f0ef8cbcf42f561032d5316
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-10-31 22:01:29 +1100

    Added alphabetic ordering of tests by location, making results easier to read

--- conftest.py
+++ conftest.py
@@ -4,3 +4,6 @@ def pytest_generate_tests(metafunc):
     # Pass the py.test -v flag to any verbosity parameters for tests
     if "verbosity" in metafunc.fixturenames:
         metafunc.parametrize("verbosity", [metafunc.config.option.verbose])
+
+def pytest_collection_modifyitems(items):
+    items[:] = reversed(sorted(items, key=lambda a: a.module.__name__ + a.name))
