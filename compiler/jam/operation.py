e8c571f90eef9c2023b7e9423a0ad54ead6f1bba
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-11 15:51:58 +1000

    Removed dead code

--- compiler/jam/operation.py
+++ compiler/jam/operation.py
@@ -7,11 +7,10 @@ class Operation(lekvar.Call):
         try:
             lekvar.Call.verify(self)
         except TypeError as e:
-            if e.__traceback__.tb_frame.f_code is self.verify.__code__:
-                if isinstance(self.called, lekvar.Attribute):
-                    raise TypeError(message="TODO: write this")
-                elif isinstance(self.called, lekvar.Identifier):
-                    raise TypeError(message="TODO: write this")
+            if isinstance(self.called, lekvar.Attribute):
+                raise TypeError(message="TODO: write this")
+            elif isinstance(self.called, lekvar.Identifier):
+                raise TypeError(message="TODO: write this")
             raise
 
 lekvar.Operation = Operation
