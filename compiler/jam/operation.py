8f3a575290ba72dfe577280865a71c5bdbd1f217
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-06-01 17:46:33 +1000

    Made operation error messages additive, rather than overriding the existing error

--- compiler/jam/operation.py
+++ compiler/jam/operation.py
@@ -8,9 +8,9 @@ class Operation(lekvar.Call):
             lekvar.Call.verify(self)
         except TypeError as e:
             if isinstance(self.called, lekvar.Attribute):
-                raise TypeError(message="TODO: write this")
+                raise e.add(message="TODO: write this")
             elif isinstance(self.called, lekvar.Identifier):
-                raise TypeError(message="TODO: write this")
+                raise e.add(message="TODO: write this")
             raise
 
 lekvar.Operation = Operation
