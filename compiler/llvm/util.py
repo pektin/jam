fcf042d2fa44dab3310c528963dbb4354f01b290
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-07-21 12:08:44 +0200

    Separated out common backend util functions from llvm

--- compiler/llvm/util.py
+++ compiler/llvm/util.py
@@ -1,13 +1,6 @@
-from .. import lekvar
 from . import bindings as llvm
 
-# Resolves the name of a scope, starting with a extraneous .
-def resolveName(scope:lekvar.BoundObject):
-    name = ""
-    while scope.bound_context is not None:
-        name = "." + scope.name + name
-        scope = scope.parent
-    return "lekvar" + name
+from ..backend.util import *
 
 # Create a reference counted type from a normal type
 def referenceType(type):
@@ -21,10 +14,4 @@ def emitValue(value, type):
 def emitAssignment(value, type):
     return value.resolveType().emitInstanceAssignment(value, type)
 
-# Mokeypatch a function into a lekvar class
-# The lekvar class is determined from the name of the function, which should be
-# in the following format: <class-name>_<function-name>
-def patch(fn):
-    class_name, func_name = fn.__name__.split("_")
-    class_ = getattr(lekvar, class_name)
-    setattr(class_, func_name, fn)
+
