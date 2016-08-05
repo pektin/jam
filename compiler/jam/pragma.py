d2521fa76409a515b16153e582d543808ee1de3a
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-08-05 20:24:55 +1000

    Made pragma's not repeatable

--- compiler/jam/pragma.py
+++ compiler/jam/pragma.py
@@ -1,13 +1,28 @@
+from contextlib import ExitStack
+
 from .. import errors
 from .. import lekvar
 from .. import interpreter
 
 class Pragma(lekvar.BoundLink):
+    has_run = False
+
     def verify(self):
         self.value.verify()
 
+        if self.has_run: return
+        self.has_run = True
+        self._run()
+
+    def _run(self):
+        interpreter.State.stdout = None
         with lekvar.useBackend(interpreter):
             self.value = self.value.eval()
-            print(interpreter.State.stdout, end="")
+
+            if interpreter.State.stdout:
+                print(interpreter.State.stdout, end="")
+
+    def eval(self):
+        pass
 
 lekvar.Pragma = Pragma
