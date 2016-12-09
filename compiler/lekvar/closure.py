6a50c01849b4200e77258e643d1dbf6a5f1a5c30
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-12-10 00:29:42 +1100

    Made ClosedTarget objects propagate on subobjects and target for checks

--- compiler/lekvar/closure.py
+++ compiler/lekvar/closure.py
@@ -1,7 +1,7 @@
-from contextlib import contextmanager
+from contextlib import contextmanager, ExitStack
 
 from .core import Object, Context, BoundObject, SoftScope, Scope
-from .links import Link, BoundLink
+from .links import Link, BoundLink, ContextLink
 
 class Closure(Scope):
     closed_context = None
@@ -34,23 +34,80 @@ class ClosedLink(BoundLink):
         return "C({})".format(str(self.value))
 
 class ClosedTarget(Link):
+    origin = None
     targets = None
 
-    def __init__(self, value:Object, targets:[(Object, Object)]):
+    def __init__(self, value:Object, targets:[(Object, Object)], origin:Object = None):
         Link.__init__(self, value)
 
         self.targets = targets
+        self.origin = origin or value
 
     @contextmanager
     def target(self):
-        oldValues = []
+        stack = ExitStack()
 
         for target, value in self.targets:
-            target = target.resolveValue()
-            oldValues.append(target.value)
-            target.value = value
+            stack.enter_context(target.resolveValue().targetValue(value))
 
-        yield
+        with stack:
+            yield
 
-        for (target, new_value), old_value in zip(self.targets, oldValues):
-            target.resolveValue().value = old_value
+    def retarget(self, value:Object):
+        return ClosedTarget(value, self.targets, self.value)
+
+    def resolveIdentifier(self, name:str):
+        with self.target():
+            return self.retarget(self.value.resolveIdentifier(name))
+
+    def resolveCall(self, call):
+        return self.retarget(self.value.resolveCall(call))
+
+    @property
+    def context(self):
+        return ClosedTargetContext(self.value.context, self)
+
+    @property
+    def instance_context(self):
+        return ClosedTargetContext(self.value.instance_context, self)
+
+    @property
+    def return_type(self):
+        return self.retarget(self.value.return_type)
+
+    def resolveType(self):
+        return self.retarget(self.value.resolveType())
+
+    def checkCompatibility(self, other, check_cache = None):
+        with self.target():
+            return self.value.checkCompatibility(other, check_cache)
+
+    def revCheckCompatibility(self, other, check_cache = None):
+        with self.target():
+            return self.value.revCheckCompatibility(other, check_cache)
+
+    def __repr__(self):
+        return "CT({})".format(self.value)
+
+    def resolveValue(self):
+        return self
+
+class ClosedTargetContext(ContextLink):
+    targeter = None
+
+    def __init__(self, value, targeter):
+        ContextLink.__init__(self, value)
+        self.targeter = targeter
+
+    def retarget(self, value):
+        return self.targeter.retarget(value)
+
+    def __getitem__(self, name:str):
+        return self.retarget(ContextLink.__getitem__(self, name))
+
+    def __iter__(self):
+        for item in ContextLink.__iter__(self):
+            yield self.retarget(item)
+
+    def __repr__(self):
+        return "CT({})".format(self.value)
